7c686e072298c865d22b36f5747fcb60929ddcf4
Marko Milković <mmilkovic2006@gmail.com>
2025-05-03 13:39:31 +0200

    Tech stack finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 
 import heroImg from '../../public/assets/hero.png';
+
 import typescriptIcon from '../../public/assets/frontend/typescript-icon.png';
 import reactIcon from '../../public/assets/frontend/react-icon.png';
 import nextjsIcon from '../../public/assets/frontend/nextjs-icon.png';
@@ -9,7 +10,17 @@ import tailwindcssIcon from '../../public/assets/frontend/tailwindcss-icon.png';
 import framerIcon from '../../public/assets/frontend/framer-icon.png';
 import bootstrapIcon from '../../public/assets/frontend/bootstrap-icon.png';
 
-import { Github, Linkedin, Mail } from "lucide-react";
+import nodejsIcon from '../../public/assets/backend/nodejs-icon.png';
+import mysqlIcon from '../../public/assets/backend/mysql-icon.png';
+import expressjsIcon from '../../public/assets/backend/expressjs-icon.png';
+import mongodbIcon from '../../public/assets/backend/mongodb-icon.png';
+import postmanIcon from '../../public/assets/backend/postman-icon.png';
+
+import figmaIcon from '../../public/assets/other/figma-icon.png';
+import vercelIcon from '../../public/assets/other/vercel-icon.png';
+import gitIcon from '../../public/assets/other/git-icon.png';
+
+import { CogIcon, Github, Linkedin, Mail, Slack } from "lucide-react";
 import { motion } from "framer-motion";
 import {Roboto, Anton} from 'next/font/google';
 import { NumberTicker } from "@/components/magicui/number-ticker";
@@ -18,6 +29,9 @@ import { BoxReveal } from "@/components/magicui/box-reveal";
 import { BlurFade } from "@/components/magicui/blur-fade";
 import NavbarItem from "./components/NavbarItem";
 import { Particles } from "@/components/magicui/particles";
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
+import SpotlightCard from "@/blocks/Components/SpotlightCard/SpotlightCard";
+import TiltedCard from "@/blocks/Components/TiltedCard/TiltedCard";
 
 
 
@@ -35,9 +49,12 @@ const anton = Anton({
 });
 
 export default function Home() {
+  const MotionSpotlightCard = motion.create(SpotlightCard);
+
   return (
     <div className="relative flex flex-col w-full h-full bg-[#121A2A]">
       <Particles quantity={50} className="fixed w-full"/>
+      <SmoothCursor />
       <div className="flex flex-col fixed bottom-5 gap-3 w-fit px-6">
         <a href="https://github.com/milkoviicc" className="text-[#C27AFF]" target="_blank"><Github size={32}/></a>
         <a href="https://linkedin.com/in/milkoviicc" className="text-[#C27AFF]" target="_blank"><Linkedin size={32}/></a>
@@ -75,7 +92,6 @@ export default function Home() {
 
 
       <div className="w-full h-full flex">
-
         <div className="container mx-auto flex justify-center items-center gap-24">
           <div className="flex items-center gap-4">
             <h2 className={`${anton.className} font-bold text-6xl text-[#C27AFF]`}><NumberTicker value={25} className="text-[#C27AFF]" />+</h2>
@@ -91,56 +107,86 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="container mx-auto px-4 flex flex-col gap-8 py-16 ">
-        <h2 className={`uppercase text-purple-500 ${anton.className} text-6xl`}>My tech stack</h2>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="flex flex-col justify-center text-center py-4 rounded-lg">
-            <motion.h3 className={`uppercase ${anton.className} text-shadow text-[#DEDEDE] text-4xl group-hover:text-fill`} whileHover={{ color: "#C27AFF" }} transition={{ duration: 0.3 }}>Frontend</motion.h3>
-            <div className="grid grid-cols-3 gap-4 mt-2">
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={typescriptIcon} alt="Typescript" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>Typescript</p>
+      <div className="container mx-auto px-4 flex flex-col gap-8 py-32 ">
+        <h2 className={`uppercase text-purple-500 ${anton.className} text-3xl flex gap-4`}>Tech stack <Slack size={32} /></h2>
+        <div className="grid grid-cols-3 gap-4 -mx-8 h-full">
+          <TiltedCard>
+            <MotionSpotlightCard className="group flex flex-col text-left py-4 rounded-lg border-0 bg-transparent h-full" spotlightColor="rgba(0, 229, 255, 0.2)">
+              <h3 className={`uppercase ${anton.className} text-shadow text-[#DEDEDE] text-6xl group-hover:text-[#C27AFF] duration-300 ease-in-out`}>Frontend</h3>
+              <div className="col-span-3 flex flex-wrap gap-x-8 gap-12 mt-10">
+                <div className="flex items-center gap-2">
+                  <Image src={tailwindcssIcon} alt="Tailwind CSS"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Tailwind CSS</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={reactIcon} alt="React"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>React</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={nextjsIcon} alt="NextJS"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>NextJS</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={typescriptIcon} alt="Typescript"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Typescript</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={framerIcon} alt="Framer"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Framer</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={bootstrapIcon} alt="Bootstrap"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Bootstrap</p>
+                </div>
               </div>
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={reactIcon} alt="React" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>React</p>
+            </MotionSpotlightCard>
+          </TiltedCard>
+          <TiltedCard>
+            <MotionSpotlightCard className="group flex flex-col text-left py-4 rounded-lg bg-transparent border-0 h-full" spotlightColor="rgba(0, 229, 255, 0.2)">
+              <h3 className={`uppercase ${anton.className} text-shadow text-[#DEDEDE] text-6xl group-hover:text-[#C27AFF] duration-300 ease-in-out`}>Backend</h3>
+              <div className="col-span-3 flex flex-wrap gap-x-8 gap-12 mt-10">
+                <div className="flex items-center gap-2 ">
+                  <Image src={nodejsIcon} alt="Typescript"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>NodeJS</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={mysqlIcon} alt="React"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>MySQL</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={expressjsIcon} alt="NextJS"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>ExpressJS</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={mongodbIcon} alt="Tailwind CSS"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>MongoDB</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={postmanIcon} alt="Framer"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Postman</p>
+                </div>
               </div>
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={nextjsIcon} alt="NextJS" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>NextJS</p>
+            </MotionSpotlightCard>
+          </TiltedCard>
+          <TiltedCard>
+            <MotionSpotlightCard className="group flex flex-col text-left py-4 rounded-lg bg-transparent border-0 h-full" spotlightColor="rgba(0, 229, 255, 0.2)">
+              <h3 className={`uppercase ${anton.className} text-shadow text-[#DEDEDE] text-6xl group-hover:text-[#C27AFF] duration-300 ease-in-out`}>Design & other</h3>
+              <div className="col-span-3 flex flex-wrap gap-x-8 gap-12 mt-10">
+                <div className="flex items-center gap-2 ">
+                  <Image src={figmaIcon} alt="Typescript"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Figma</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={vercelIcon} alt="React"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Vercel</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Image src={gitIcon} alt="NextJS"/>
+                  <p className={`text-[#DEDEDE] ${roboto.className} text-xl`}>Git</p>
+                </div>
               </div>
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={tailwindcssIcon} alt="Tailwind CSS" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>Tailwind CSS</p>
-              </div>
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={framerIcon} alt="Framer" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>Framer</p>
-              </div>
-              <div className="flex flex-col gap-2 items-center justify-center">
-              <Image src={bootstrapIcon} alt="Bootstrap" width={64} height={64} className="w-16 h-16"/>
-              <p className={`text-[#DEDEDE] ${roboto.className} text-lg`}>Bootstrap</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center text-center py-4 bg-white/5 border-[1px] border-white/20 bakdrop-blur-2xl rounded-lg">
-            <h3 className={`uppercase ${anton.className} text-[#DEDEDE] text-3xl`}>Frontend</h3>
-            <div className="grid grid-cols-3 gap-4 mt-2">
-              <div className="flex flex-col items-center justify-center">
-                <Image src={typescriptIcon} alt="Typescript" width={64} height={64} className="w-16 h-16"/>
-                <p className={`text-[#DEDEDE] ${roboto.className} text-sm`}>Typescript</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center text-center py-4 bg-white/5 border-[1px] border-white/20 bakdrop-blur-2xl rounded-lg">
-            <h3 className={`uppercase ${anton.className} text-[#DEDEDE] text-3xl`}>Frontend</h3>
-            <div className="grid grid-cols-3 gap-4 mt-2">
-              <div className="flex flex-col items-center justify-center">
-                <Image src={typescriptIcon} alt="Typescript" width={64} height={64} className="w-16 h-16"/>
-                <p className={`text-[#DEDEDE] ${roboto.className} text-sm`}>Typescript</p>
-              </div>
-            </div>
-          </div>
+            </MotionSpotlightCard>
+          </TiltedCard>
         </div>
       </div>
     </div>
---
+++ src/blocks/Components/TiltedCard/TiltedCard.tsx
@@ -0,0 +1,123 @@
+import type { SpringOptions } from "framer-motion";
+import { useRef, useState } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+interface TiltedCardProps {
+  containerHeight?: React.CSSProperties["height"];
+  containerWidth?: React.CSSProperties["width"];
+  scaleOnHover?: number;
+  rotateAmplitude?: number;
+  showMobileWarning?: boolean;
+  showTooltip?: boolean;
+  overlayContent?: React.ReactNode;
+  displayOverlayContent?: boolean;
+  children?: React.ReactNode;
+}
+
+const springValues: SpringOptions = {
+  damping: 30,
+  stiffness: 100,
+  mass: 2,
+};
+
+export default function TiltedCard({
+  containerHeight = "300px",
+  containerWidth = "100%",
+  scaleOnHover = 1.1,
+  rotateAmplitude = 14,
+  showMobileWarning = true,
+  overlayContent = null,
+  displayOverlayContent = false,
+  children,
+}: TiltedCardProps) {
+  const ref = useRef<HTMLElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useSpring(useMotionValue(0), springValues);
+  const rotateY = useSpring(useMotionValue(0), springValues);
+  const scale = useSpring(1, springValues);
+  const opacity = useSpring(0);
+  const rotateFigcaption = useSpring(0, {
+    stiffness: 350,
+    damping: 30,
+    mass: 1,
+  });
+
+  const [lastY, setLastY] = useState(0);
+
+  function handleMouse(e: React.MouseEvent<HTMLElement>) {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left - rect.width / 2;
+    const offsetY = e.clientY - rect.top - rect.height / 2;
+
+    const rotationX = (offsetY / (rect.height / 2)) * -rotateAmplitude;
+    const rotationY = (offsetX / (rect.width / 2)) * rotateAmplitude;
+
+    rotateX.set(rotationX);
+    rotateY.set(rotationY);
+
+    x.set(e.clientX - rect.left);
+    y.set(e.clientY - rect.top);
+
+    const velocityY = offsetY - lastY;
+    rotateFigcaption.set(-velocityY * 0.6);
+    setLastY(offsetY);
+  }
+
+  function handleMouseEnter() {
+    scale.set(scaleOnHover);
+    opacity.set(1);
+  }
+
+  function handleMouseLeave() {
+    opacity.set(0);
+    scale.set(1);
+    rotateX.set(0);
+    rotateY.set(0);
+    rotateFigcaption.set(0);
+  }
+
+  return (
+    <figure
+      ref={ref}
+      className="relative w-full h-full [perspective:800px] flex flex-col items-center justify-center"
+      style={{
+        height: containerHeight,
+        width: containerWidth,
+      }}
+      onMouseMove={handleMouse}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {showMobileWarning && (
+        <div className="absolute top-4 text-center text-sm block sm:hidden">
+          This effect is not optimized for mobile. Check on desktop.
+        </div>
+      )}
+
+      <motion.div
+        className="relative [transform-style:preserve-3d]"
+        style={{
+          width: "100%",
+          height: "100%",
+          rotateX,
+          rotateY,
+          scale,
+        }}
+      >
+        {children && (
+          <motion.div className="absolute top-0 left-0 w-full h-full">{children}</motion.div>
+        )}
+
+        {displayOverlayContent && overlayContent && (
+          <motion.div className="absolute top-0 left-0 z-[2] will-change-transform [transform:translateZ(30px)]">
+            {overlayContent}
+          </motion.div>
+        )}
+      </motion.div>
+
+    </figure>
+  );
+}
