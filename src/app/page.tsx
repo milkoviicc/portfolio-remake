69a5de5b2bcafa0a50daa741bab75f968540e49c
Marko Milković <mmilkovic2006@gmail.com>
2025-07-29 12:07:52 +0200

    Updated projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -379,8 +379,8 @@ export default function Home() {
 
           <AnimatePresence>
             {showProjectImage === 'xemtio' && (
-              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-0 transform -translate-y-1/2 hidden md:block">
-                <Image src={xemtio} width={650} height={650} alt="Xemtio project preview" className="rounded-lg" />
+              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-4 transform -translate-y-1/2 hidden xl:block">
+                <Image src={xemtio} width={650} height={650} alt="Xemtio project preview" className="rounded-lg w-[550px] 2xl:w-[650px]" />
               </motion.div>
             )}
           </AnimatePresence>
@@ -395,8 +395,8 @@ export default function Home() {
 
           <AnimatePresence>
             {showProjectImage === 'snet' && (
-              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-0 transform -translate-y-1/2 hidden md:block">
-                <Image src={snet} width={650} height={650} alt="SNET project preview" className="rounded-lg"/>
+              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-4 z-[100] transform -translate-y-1/2 hidden xl:block">
+                <Image src={snet} alt="SNET project preview" className="rounded-lg w-[550px] 2xl:w-[650px]"/>
               </motion.div>
             )}
           </AnimatePresence>
@@ -405,14 +405,15 @@ export default function Home() {
           <motion.div onClick={() => window.open('https://opgnedaca.vercel.app', '_blank')} onHoverStart={() => setShowProjectImage('opgnedaca')} onHoverEnd={() => setShowProjectImage(null)} className="group" initial={{ opacity: 0, y: 50 }} whileInView={{opacity: 1, y: 0}} transition={{ duration: 0.5, delay: 0.2 }}>
             <SpotlightCard className="flex flex-col text-left py-2 px-4 md:px-6 rounded-lg bg-transparent border-0 w-full max-w-[500px] md:w-[650px] md:max-w-full h-full z-50" spotlightColor="rgba(0, 229, 255, 0.2)">
               <h3 className={`text-3xl lg:text-6xl font-bold uppercase text-[#DEDEDE] ${roboto.className} transition-all duration-700 bg-gradient-to-r from-[#C27AFF] to-[#DEDEDE] from-[50%] to-[50%] bg-[length:200%] bg-right bg-clip-text text-transparent group-hover:bg-left flex justify-between items-center`}>OPG Nedaća<span className={`${ultra.className} text-3xl`}>3.</span></h3>
-              <p className={`text-sm lg:text-lg font-extralight ${roboto.className} w-[90%] lg:w-full text-[#DEDEDE] mt-2 lg:mt-8`}>OPG Nedaća is a family-owned organic farm in Croatia, focused on growing eco-friendly fruit and producing high-quality natural products. The website is built with React and features a cart-based ordering system.</p>
+              <h3 className="text-lg text-gray-500 pt-2">STILL IN PROGRESS</h3>
+              <p className={`text-sm lg:text-lg font-extralight ${roboto.className} w-[90%] lg:w-full text-[#DEDEDE] mt-2 lg:mt-4`}>OPG Nedaća is a family-owned organic farm in Croatia, focused on growing eco-friendly fruit and producing high-quality natural products. The website is built with React and features a cart-based ordering system.</p>
             </SpotlightCard>
           </motion.div>
 
           <AnimatePresence>
             {showProjectImage === 'opgnedaca' && (
-              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-0 transform -translate-y-1/2 hidden md:block">
-                <Image src={opgnedaca} width={650} height={650} alt="OPG Nedaća project preview" className="rounded-lg"/>
+              <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 50 }} transition={{ duration: 0.4 }} className="absolute top-1/2 right-4 transform -translate-y-1/2 hidden xl:block">
+                <Image src={opgnedaca} width={650} height={650} alt="OPG Nedaća project preview" className="rounded-lg w-[550px] 2xl:w-[650px]"/>
               </motion.div>
             )}
           </AnimatePresence>
